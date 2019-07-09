898df3849a9b43de6d80c034480d6714d53a2cc5
higaguin <higaguin123@hotmail.com>
2019-07-09 11:31:23 -0500

    first migration

--- cinema_api/models.py
+++ cinema_api/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.conf import settings
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    title = models.CharField(max_length=5)
+    dob = models.DateField()
+    address = models.CharField(max_length=255)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    zip = models.CharField(max_length=5)
+    photo = models.ImageField(upload_to='uploads', blank=True)
+
+    def __str__(self):
+        """Django uses this when it needs to convert the object to string"""
+
+        return self.user
